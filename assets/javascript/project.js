a459e525123fb9ea24aab0586dc880b1de2bb6e5
Lisa Hagin <lhagin@encompass.tv>
2017-03-14 15:58:41 -0400

    setup week 8 project

---
+++ assets/javascript/project.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {   
+	// Initialize Firebase
+var queryURL = "http://api.brewerydb.com/v2/?key=6c667709753ad53866207f52c01820c8";
+console.log(queryURL);
+  
+
+});//end document.ready

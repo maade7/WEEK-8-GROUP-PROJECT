e0456ff84ea81cac189f29063b1a8bca68a55cff
haginl1 <lhagin@comcast.net>
2017-03-14 21:03:08 -0400

    Delete project.js

--- assets/javascript/project.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {   
-	// Initialize Firebase
-var queryURL = "http://api.brewerydb.com/v2/?key=6c667709753ad53866207f52c01820c8";
-console.log(queryURL);
-  
-
-});//end document.ready
